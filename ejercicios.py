bcaef296b08d5e3861ddd69033aa66da69dc932f
Gerardouz <gerardouz13@gmail.com>
2016-09-20 23:39:02 -0400

    Update ejercicios.py

--- ejercicios.py
+++ ejercicios.py
@@ -1,3 +1,11 @@
+#invertir lista: https://repl.it/X3G/4160
+#eliminar la mitad de la lista: https://repl.it/X3G/4161
+#verificar si hay numeros repetidos en la lista: https://repl.it/X3G/4162
+#saber si dos listas son iguales: https://repl.it/X3G/4163
+#sumar los elementos de una lista multitipo: https://repl.it/X3G/4164
+
+
+
 # CLASE NODO
 class Nodo:
 	def __init__ (self, valor):
@@ -96,102 +104,109 @@ class Lista:
 		if (self.__ultimo == h):
 			
 			self.__ultimo = h.sig
-			
+		del h
 		self.__n = self.__n - 1
 		self.__pos = self.__pos - 1
-    del h
-			
-			
+
+
 	# Metodo para invertir la lista
-  def invertir_lista(self):
-    lista = []
-    nodo = sekf.__primero
-    while (nodo != None):
-      lista.append(nodo.info)
-      nodo = nodo.sig
-      self.eliminar_prim()
-    for i in lista:
-      self.insertar_inicio(i)
-      
-  # Metodo para eliminar la mitad de la lista
-  def elimina_mitad(self):
-  
-    if (self.__primero == None):
-      return
-      
-    h = self.__n / 2
-    
-    for i in range(h):
-    
-      self.eliminar_prim()
-      
-  # Metodo para buscar si hay elementos repetidos en la lista
-  
-  def numeros_repetidos(self):
-  
-    nodo = self.__primero
-    cont = 0
-    while (nodo != None):
-      valor = nodo.sig
-      
-      while (valor != None):
-        if (nodo.info == valor.info):
-          cont = cont + 1
-        valor = valor.sig
-      nodo = nodo.sig
-    
-    if (cont > 0):
-      return True
-    
-    if (cont == 0):
-      return False
-      
-  # Metodo para consultar la cantidad de elementos de una lista
-  def consulta_n(self):
-  
-    return self.__n
-    
-  # Metodo para saber si dos listas son iguales
-  
-  def listas_iguales(self,lista):
-  
-    if (type(lista) != Lista()):
-      return False
-      
-    if (self.__n != lista.consulta_n()):
-      return False
-      
-    nodo1 = self.__primero
-    nodo2 = lista.__primero
-    
-    while (nodo1 != None and nodo2 != None):
-    
-      if (nodo1.info != nodo2.info):
-      
-        return False
-        
-      nodo1 = nodo1.sig
-      nodo2 = nodo2.sig
-      
-    return True
-    
-    
-  # Metodo para sumar los elementos de una lista multitipo
-  
-  def sumar_lista(self):
-    
-    suma = 0
-    
-    nodo = self.__primero
-    
-    while (nodo != None):
-    
-      if (type(nodo.info) == int):
-        suma = suma + nodo.info
-      
-      nodo = nodo.sig
-      
-    return suma
-      
+
+	def invertir_lista(self):
+		l = []
+		nodo = self.__primero
+		while(nodo != None):
+			l.append(nodo.info)
+			nodo = nodo.sig
+			self.eliminar_prim()
+		for i in l:
+			self.insertar_inicio(i)
+
+	# Metodo para eliminar la mitad de una lista
+
+	def eliminar_mitad(self):
+
+		if (self.__primero == None):
+			return
+		h = self.__n / 2
+
+		for i in range(h):
+			self.eliminar_prim()
+
+	# Metodo para verificar si hay numeros repetidos en la lista
+
+	def numeros_repetidos(self):
+
+		nodo = self.__primero
+
+		cont = 0
+
+		while (nodo != None):
+
+			valor = nodo.sig
+			while (valor != None):
+
+				if (nodo.info == valor.info):
+
+					cont = cont + 1
+
+				valor = valor.sig
+
+			nodo = nodo.sig
+
+		if (cont > 0):
+
+			return True
+
+		if (cont == 0):
+
+			return False
+
+	# Metodo para consultar la cantidad de elementos de una lista
+
+	def consultar_n(self):
+
+		return self.__n
+
+	# Metodo para saber si dos listas son iguales
+
+	def listas_iguales(self,lista):
+
+
+		if (self.__n != lista.consultar_n() ):
+
+			return False
+
+		nodo1 = self.__primero
+		nodo2 = lista.__primero
+		cont = 0
+
+		while (nodo1 != None and nodo2 != None):
+
+			if (nodo1.info != nodo2.info):
+				return False
+
+			nodo1 = nodo1.sig
+			nodo2 = nodo2.sig
+
+		return True
+
+	# Metodo para sumar los elementos de una lista multitipo
+
+	def sumar_lista(self):
+
+		suma = 0
+
+		nodo = self.__primero
+
+		while (nodo != None):
+
+			if (type(nodo.info) == int):
+
+				suma = suma + nodo.info
+
+			nodo = nodo.sig
+
+		return suma
+
     
 			
